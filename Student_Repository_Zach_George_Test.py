735df892482abdfa6e90e4aecf4c3fc28ad1e98c
ztylergeorge <68574285+ztylergeorge@users.noreply.github.com>
2020-07-21 22:52:05 -0400

    Add files via upload

--- Student_Repository_Zach_George_Test.py
+++ Student_Repository_Zach_George_Test.py
@@ -16,11 +16,11 @@ class UniversityTest(unittest.TestCase):
         
         """ Test the University class at error handling """
         
-        good_path: str = 'C:/Users/Class2018/eclipse-workspace/HW09/src/Files'
         fake_path: str ='C:/Users/Class2018/eclipse-workspace/HW09/src/Fake' 
         missing_students_path: str = 'C:/Users/Class2018/eclipse-workspace/HW09/src/Missing Students'
         missing_grades_path: str = 'C:/Users/Class2018/eclipse-workspace/HW09/src/Missing Grades'
         missing_instructors_path: str = 'C:/Users/Class2018/eclipse-workspace/HW09/src/Missing Instructors'
+        missing_majors_path: str = 'C:/Users/Class2018/eclipse-workspace/HW09/src/Missing Majors'
         unknown_student_path: str = 'C:/Users/Class2018/eclipse-workspace/HW09/src/Unknown Student'
         unknown_instructor_path: str = 'C:/Users/Class2018/eclipse-workspace/HW09/src/Unknown Instructor'
         too_many_fields_path: str = 'C:/Users/Class2018/eclipse-workspace/HW09/src/Too Many Fields'
@@ -33,6 +33,12 @@ class UniversityTest(unittest.TestCase):
             University(missing_instructors_path)
         with self.assertRaises(ValueError):
             University(missing_grades_path)
+        with self.assertRaises(ValueError):
+            University(missing_majors_path)
+        with self.assertRaises(ValueError):
+            University(unknown_student_path)
+        with self.assertRaises(ValueError):
+            University(unknown_instructor_path)
         with self.assertRaises(ValueError):
             University(too_many_fields_path)
             
@@ -63,8 +69,34 @@ class UniversityTest(unittest.TestCase):
                                             [str(98761), 'Edison, A', 'SYEN', {}],
                                             [str(98760), 'Darwin, C', 'SYEN', {'SYS 800': 1, 'SYS 750': 1, 'SYS 611': 2, 'SYS 645': 1}]
                                              ]
-        self.assertEqual([[student._cwid, student.name, student.major, student._courses_grades] for student in test_university._students.values()], students_info)
+        students_missing_courses: List[List[str, Set[str]]] = [['10103', {'SSW 555', 'SSW 540'}],
+                                                                 ['10115', {'SSW 540', 'SSW 555'}],
+                                                                 ['10172', {'SSW 540', 'SSW 564'}],
+                                                                 ['10175', {'SSW 540', 'SSW 555'}], 
+                                                                 ['10183', {'SSW 540', 'SSW 555', 'SSW 564', 'SSW 567'}],
+                                                                 ['11399', {'SYS 612', 'SYS 671', 'SYS 800'}],
+                                                                 ['11461', {'SYS 612', 'SYS 671'}],
+                                                                 ['11658', {'SYS 612', 'SYS 671', 'SYS 800'}],
+                                                                 ['11714', {'SYS 612', 'SYS 671', 'SYS 800'}],
+                                                                 ['11788', {'SYS 612', 'SYS 671', 'SYS 800'}]
+                                                                 ]
+        
+        students_missing_electives: List[List[str, Set[str]]] = [['10103', {}],
+                                                                 ['10115', {}],
+                                                                 ['10172', {'CS 501', 'CS 513', 'CS 545'}],
+                                                                 ['10175', {'CS 501', 'CS 513', 'CS 545'}],
+                                                                 ['10183', {'CS 501', 'CS 513', 'CS 545'}],
+                                                                 ['11399', {}],
+                                                                 ['11461', {'SSW 540', 'SSW 565', 'SSW 810'}],
+                                                                 ['11658', {'SSW 540', 'SSW 565', 'SSW 810'}],
+                                                                 ['11714', {'SSW 540', 'SSW 565', 'SSW 810'}],
+                                                                 ['11788', {}]
+                                                                 ]
+                                         
+        self.assertEqual([[student._cwid, student.name, student.major.name, student._courses_grades] for student in test_university._students.values()], students_info)
         self.assertEqual([[instructor._cwid, instructor.name, instructor.department, instructor._courses_students] for instructor in test_university._instructors.values()], instructors_info)
+        self.assertEqual([[student._cwid, student._remaining_courses] for student in test_university._students.values()], students_missing_courses)
+        self.assertEqual([[student._cwid, student._remaining_electives] for student in test_university._students.values()], students_missing_electives)
         
                     
 if __name__ == '__main__':
